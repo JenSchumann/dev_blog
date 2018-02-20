d6235ea429611005e29aed693f007eb10e44c38a
Jen Schumann <jenniferschumann1@gmail.com>
2018-02-19 17:40:07 -0700

    adding admin login functionality

---
+++ models/admin.js
@@ -0,0 +1,20 @@
+//dependencies
+const mongoose = require('mongoose');
+const Post = require('./post.js');
+
+
+//Schema
+const adminSchema = mongoose.Schema({
+
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+
+  post: [Post.schema],
+
+});
+
+const Admin = mongoose.model('Admin', adminSchema);
+
+
+//export
+module.exports = Admin;
